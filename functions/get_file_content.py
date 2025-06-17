f438abca954c3dd14a1fedfd42461687dade5325
Ronin <ronin@device.local>
2025-06-17 00:23:32 -0600

    chore: update variable name

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -2,17 +2,17 @@ import os
 
 def get_file_content(working_directory, file_path): 
     working_directory_abs_path = os.path.abspath(working_directory)
-    path = os.path.abspath(os.path.join(working_directory_abs_path, file_path))
+    full_path = os.path.abspath(os.path.join(working_directory_abs_path, file_path))
 
-    if not path.startswith(working_directory_abs_path): 
+    if not full_path.startswith(working_directory_abs_path): 
         return (f'Error: Cannot read "{file_path}" as it is outside the permitted working directory')
-    if not os.path.isfile(path):
+    if not os.path.isfile(full_path):
         return (f'Error: File not found or is not a regular file: "{file_path}"')
     
     MAX_CHARS = 10000
 
     try: 
-        with open(path,'r') as f:
+        with open(full_path,'r') as f:
             file_content_string = f.read(MAX_CHARS)
             if(len(file_content_string)== MAX_CHARS): 
                 file_content_string += f'[...File "{file_path}" truncated at 10000 characters]'
--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -5,17 +5,17 @@ def get_files_info(working_directory, directory=None):
     dir_name = directory.strip('/')
     working_directory_abs_path = os.path.abspath(working_directory)
     
-    path = os.path.abspath(os.path.join(working_directory_abs_path, dir_name))
+    full_path = os.path.abspath(os.path.join(working_directory_abs_path, dir_name))
 
-    if not path.startswith(working_directory_abs_path): 
+    if not full_path.startswith(working_directory_abs_path): 
         return (f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
-    if not os.path.isdir(path): 
+    if not os.path.isdir(full_path): 
         return f'Error: "{directory}" is not a directory'
-    dir_content = os.listdir(path)
+    dir_content = os.listdir(full_path)
     metadata_str = ""
     
     for item_name in dir_content: 
-        item_path = os.path.join(path,item_name)
+        item_path = os.path.join(full_path,item_name)
         try: 
             item_metadata = os.stat(item_path)
             metadata_str += f"- {item_name} file_size={item_metadata.st_size} bytes, is_dir={os.path.isdir(item_path)}\n" 
