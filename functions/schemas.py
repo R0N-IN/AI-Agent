c10da1ea88ea0a0084f7c3506a34e43fbec6b91a
Ronin <ronin@device.local>
2025-07-14 22:33:24 -0600

    Add schemas for all available functions for the agent

--- functions/schemas.py
+++ functions/schemas.py
@@ -15,6 +15,51 @@ schema_get_files_info = types.FunctionDeclaration(
     ),
 )
 
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="List the full content of the specified file",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file to get the content from, relative to the working directory. If not provided just return the result",
+            ),
+        },
+    ),
+)
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Run the specified python file if you don't get the path, just try to run it",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file to be run, relative to the working directory. If not provided just return the result",
+            ),
+        },
+    ),
+)
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Run the specified python file using python3 'name of the file' syntax",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file to get the content from, relative to the working directory. If not provided just return the result",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The content to be written on the file",
+            ),
+        },
+    ),
+)
 # schema_get_files_info = types.FunctionDeclaration(
 #     name="get_file_content",
 #     description="Store the file content in a string, if the number of characters exceeds 10000 it truncates the file.",
--- main.py
+++ main.py
@@ -14,12 +14,15 @@ client = genai.Client(api_key=api_key)
 try: 
     prompt = sys.argv[1]
 except IndexError:
-    print("Usage: python main.py '<your prompt here>'")
+    print("Usage: uv run main.py '<your prompt here>'")
     sys.exit(1)
 
 available_functions = types.Tool(
     function_declarations=[
         schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file
     ]
 )
 # Store conversation history
@@ -34,17 +37,19 @@ response = client.models.generate_content(
     config=types.GenerateContentConfig(tools=[available_functions],system_instruction=os.environ.get("system_prompt")))
 
 # Check if the response contains function calls
-function_call_part = response.function_calls[0]
-function_call_content = response.candidates[0].content
-
+try:
+    function_call_part = response.function_calls[0]
+    function_call_content = response.candidates[0].content
+except: 
+    function_call_part = None
+    function_call_content = None
 # Optionally print verbose output
+if(function_call_part):
+    #print(f'___________{function_call_part}')
+    print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 if "--verbose" in sys.argv:
     print(f"User prompt: {prompt}")
-    if(function_call_part):
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-        print(f"Response: {response.text}")
-    else:
-        print(f"Response: {response.text}")
+    print(f"Response: {response.text}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 else:
